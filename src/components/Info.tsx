5a8706b0447d473784800c85ce2e4a767cf4a46e
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-01-15 18:06:37 -0300

    added background

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,6 +3,11 @@ type Props = {}
 export default function Info({ }: Props) {
 
     return <section className="info">
+        <div className="info-background">
+            <div className="ball-1"/>
+            <div className="ball-2"/>
+            <div className="ball-3"/>
+        </div>
         This information is provisional
     </section>
 }
